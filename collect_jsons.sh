da0d88aa5ef21ea84e78c787d3f34aecce52e966
chrispyatt <chris-pyatt@live.co.uk>
2021-06-16 16:56:28 +0100

    myeloid bug fixed, upload logic complete (including database addition

--- collect_jsons.sh
+++ collect_jsons.sh
@@ -3,34 +3,38 @@
 # housekeeping - ensure we're in the correct directory OR add option so user can command script
 # to download to specific place
 
+# This script collects all new multiqc jsons from DNAnexus, for upload to MegaQC. You'll need to be logged into DNAnexus.
 
+
+# Standardise default separator.
 IFS=$'\n'
-
-# need database (flatfile for simplicity - sqlite?)
-# database to hold filenames or checksums(?) of reports already uploaded to megaqc
-
-# each assay has different project name convention (or subtitle?) so needs separate 'find' block
-# save to assay-specific directory for ease later
-
 
 # Directory to save downloaded JSON files into. Change according to policy.
 JSONPATH=/MegaQC-jsons
 
 
+# Function to do the work since we're doing basically the same thing for each assay.
 download_files () {
-    for i in $(dx find projects --name "002_*_$1_*"); do
+    # Find all 002 projects with appropriate suffix. '$1' and '$2' refer to arguments given when this function is called below.
+    for i in $(dx find projects --name "002_*_$1"); do
     projectID=$(printf $i | awk -F " " '{print $1}')
     projectName=$(printf $i | awk -F " " '{print $3}')
+        # Find all multiqc json files within each project. 'dirName' is the 'single' runfolder.
         for j in $(dx find data --name "multiqc_data.json" --project $projectID); do
-            dirName=$(printf $j | awk -F " " '{print $6}' | awk -F "/" '{print $6}')
+            if [ $1 = MYE ]; then
+                dirName=$(printf $j | awk -F " " '{print $6}' | awk -F "/" '{print $5}')
+            else;
+                dirName=$(printf $j | awk -F " " '{print $6}' | awk -F "/" '{print $6}')
+            fi
             fileID=$(printf $j | awk -F " " '{print $7}' | tr -d '()')
             # Check for previous uploads (to prevent repeated downloads = cost)
             if [ $(sqlite3 MegaQC_Uploads.sqlite "SELECT EXISTS(SELECT 1 FROM megaQC_jsons WHERE DNAnexus_fileID=$fileID LIMIT 1);") -eq 0 ]; then 
                 mkdir -p $JSONPATH/$2/$projectName/$dirName; dx download $fileID -o $JSONPATH/$2/$projectName/$dirName
-                json=$(ls $JSONPATH/$2/$projectName/$dirName) #THIS WON'T WORK IF MULTIPLE!! JUST A PLACEHOLDER
-                # Check JSON to see if it's a proper run (i.e. more than 5 samples - threshold set by policy)
+                json=$(ls $JSONPATH/$2/$projectName/$dirName) # Works as long as there's only one json per 'single' runfolder
+                # Check JSON to see if it's a proper run (i.e. more than 5 samples - threshold set by policy). Indexing the json by keyname didn't work.
+                # This just counts the number of entries (samples) in the first section of 'report_general_stats_data'. Insert into DB if acceptable.
                 if [ $(jq ''[.[]]'[2][0] | length' $json) -gt 5 ]; then
-                    sqlite3 MegaQC_Uploads.sqlite "INSERT INTO megaQC_jsons (Runfolder, DNAnexus_fileID, Uploaded) VALUES ($projectName, $fileID, '0');"
+                    sqlite3 MegaQC_Uploads.sqlite "INSERT INTO megaQC_jsons (Runfolder, DNAnexus_fileID, Uploaded) VALUES ($dirName, $fileID, '0');"
                 fi
             fi
         done
@@ -38,7 +42,7 @@ download_files () {
 }
 
 
-# download myeloid files (project name suffix is MYE)
+# download myeloid files (project name suffix is MYE)              BUG FIXED: DIRECTORY STRUCTURE IS DIFFERENT
 download_files 002_*_MYE_* MYELOID
 
 # download FH files (project name suffix is FHC)
--- upload_jsons.sh
+++ upload_jsons.sh
@@ -4,20 +4,27 @@
 # duplicate uploads should not be present if download script is working
 # but megaqc will ignore attempts with identical config creation dates (or some other metadata)
 
-
+# Each sequencer (instrument) will be associated with a unique megaqc user. Each of these users will run this script, giving the sequencer ID as an argument.
 # pass instrument to script at command line
 INSTRUMENT=$1
 
 
 # Directory where JSONS are saved (policy)
-JSONPATH=/MegaQC-jsons
+#JSONPATH=/MegaQC-jsons
+JSONPATH=~/Dev/megaQC/data
 
 
 # need upload block for each assay and each instrument (to allow for different upload user & filtering in megaqc dashboards)
+INSTRUMENT_FOLDERS=$JSONPATH/*_${INSTRUMENT}_*/*/
 
 
-for report in $(find $JSONPATH/* -name '_$INSTRUMENT_*/*.json'); do 
-    megaqc upload $report
+for runfolder in $INSTRUMENT_FOLDERS; do
+    dir=$(basename $report)
+    for report in $(find $runfolder -name '*.json'); do
+        # Upload report to MegaQC
+        megaqc upload $report
+        # Update upload database to show file as uploaded.
+        sqlite3 MegaQC_Uploads.sqlite "UPDATE megaQC_jsons SET Uploaded=1 WHERE Runfolder=$dir;"
+    done
 done
 
-# add name/ID to upload database
