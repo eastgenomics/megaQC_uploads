b0275e33cbf1c1f6aff50d9ba2de5aa148aa6f0b
chrispyatt <chris-pyatt@live.co.uk>
2021-06-23 16:32:24 +0100

    pattern corrected

--- collect_jsons.sh
+++ collect_jsons.sh
@@ -16,7 +16,7 @@ JSONPATH=/MegaQC-jsons
 # Function to do the work since we're doing basically the same thing for each assay.
 download_files () {
     # Find all 002 projects with appropriate suffix. '$1' and '$2' refer to arguments given when this function is called below.
-    for i in $(dx find projects --name "002_*_$1"); do
+    for i in $(dx find projects --name $1); do
     projectID=$(printf $i | awk -F " " '{print $1}')
     projectName=$(printf $i | awk -F " " '{print $3}')
         # Find all multiqc json files within each project. 'dirName' is the 'single' runfolder.
@@ -43,13 +43,13 @@ download_files () {
 
 
 # download myeloid files (project name suffix is MYE)              BUG FIXED: DIRECTORY STRUCTURE IS DIFFERENT
-download_files 002_*_MYE_* MYELOID
+download_files 002_*_MYE MYELOID
 
 # download FH files (project name suffix is FHC)
-download_files 002_*_FHC_* FH
+download_files 002_*_FHC FH
 
 # download WES files (project name suffix is TWE)
-download_files 002_*_TWE_* WES
+download_files 002_*_TWE WES
 
 # download WES files (project name has no suffix)
 download_files 002_*_clinicalgenetics TSOE
